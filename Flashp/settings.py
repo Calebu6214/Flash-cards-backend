a8c6a4b9304b33722173f67ee2bc92bfb5fa0520
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-26 11:57:29 +0300

    add urls.py file

--- Flashp/settings.py
+++ Flashp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flashapp',
     'rest_framework',
     'rest_framework.authtoken'
 ]
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Flashp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flashcards',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'127.0.0.1',
+        'PORT':'', 
     }
 }
 
