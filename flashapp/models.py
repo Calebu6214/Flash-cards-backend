2d3f3343910059e2cbfe315d3236331de48bdbdd
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-28 09:53:23 +0300

    add model function

--- flashapp/models.py
+++ flashapp/models.py
@@ -9,32 +9,32 @@ class Subject(models.Model):
     description = models.TextField()
     
 
-    # def __str__(self):
-    #     return self.subject
+    def __str__(self):
+        return self.subject
     
-    # def save_subject(self):
-    #     self.save()
+    def save_subject(self):
+        self.save()
 
     
-    # @classmethod
-    # def get_subject_by_id(cls,id):
-    #     subject = cls.objects.get(id=id)
-    #     return subject
+    @classmethod
+    def get_subject_by_id(cls,id):
+        subject = cls.objects.get(id=id)
+        return subject
 
-    # @classmethod
-    # def get_subject(cls,id):
-    #     subject = cls.objects.get(pk=id)
-    #     return subject
+    @classmethod
+    def get_subject(cls,id):
+        subject = cls.objects.get(pk=id)
+        return subject
 
-    # @classmethod
-    # def delete_subject(cls,id):
-    #     cls.objects.filter(id).delete()
+    @classmethod
+    def delete_subject(cls,id):
+        cls.objects.filter(id).delete()
 
-    # @classmethod
-    # def display_subject(cls):
-    #     subject = cls.objects.all()
-    #     return subject
+    @classmethod
+    def display_subject(cls):
+        subject = cls.objects.all()
+        return subject
 
-    # @classmethod
-    # def update_subject(cls,id,new_update):
-    #     cls.objects.filter(id=id).update(subject=new_update)
+    @classmethod
+    def update_subject(cls,id,new_update):
+        cls.objects.filter(id=id).update(subject=new_update)
